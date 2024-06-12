e5dec35359e89d9fec258b7c758dc6a661802a9f
JunChen22 <ccjh522@gmail.com>
2024-06-11 23:45:12 -0400

    update maven pom files

--- search/src/main/java/com/itsthatjun/media/SearchApplication.java
+++ search/src/main/java/com/itsthatjun/media/SearchApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SearchApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         SpringApplication.run(SearchApplication.class, args);
     }
 }
